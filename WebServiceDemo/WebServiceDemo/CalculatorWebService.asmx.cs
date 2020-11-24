f4834d78202bc5609947ac5ab8b192b7120bd430
amolawaetb <37067791+amolawaetb@users.noreply.github.com>
2020-11-24 18:50:41 +0000

    metod overloading web method attributes

--- WebServiceDemo/WebServiceDemo/CalculatorWebService.asmx.cs
+++ WebServiceDemo/WebServiceDemo/CalculatorWebService.asmx.cs
@@ -10,7 +10,7 @@ namespace WebServiceDemo
     /// Summary description for CalculatorWebService
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
-    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
@@ -18,7 +18,7 @@ namespace WebServiceDemo
     {
         List<string> calculations; //to save recent calculations
 
-        [WebMethod (EnableSession = true)]
+        [WebMethod (EnableSession = true, Description ="Add 2 Numbers", BufferResponse = true, CacheDuration = 20,  MessageName = "Add2Numbers")]
         public int Add(int num1, int num2)
         {
             if(Session["Calc"] == null)
@@ -38,7 +38,27 @@ namespace WebServiceDemo
             return num1 + num2;
         }
 
-        [WebMethod(EnableSession = true)]
+        [WebMethod(EnableSession = true, Description = "Add 3 Numbers", BufferResponse = true, CacheDuration = 20, MessageName = "Add3Numbers")]
+        public int Add(int num1, int num2, int num3)
+        {
+            if (Session["Calc"] == null)
+            {
+                calculations = new List<string>(); //if first time create new List
+            }
+            else
+            {
+                calculations = (List<string>)Session["Calc"]; //if sessions is availble use that
+            }
+
+            string strRecentCala = num1.ToString() + " + " + num2.ToString() + " + " + num3.ToString() + " = " + (num1 + num2 + num3).ToString();
+            calculations.Add(strRecentCala);
+
+            Session["Calc"] = calculations;
+
+            return num1 + num2;
+        }
+
+        [WebMethod(EnableSession = true, CacheDuration = 20)]
         public List<string> GetCalc()
         {
             if(Session["Calc"] == null)
